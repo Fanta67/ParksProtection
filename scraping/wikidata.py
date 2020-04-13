f17c4b8d1e48a85833c8025a515da16c19959f7d
Dylan Kan <dylan.kan67@utexas.edu>
2020-04-13 17:02:21 -0500

    final wikidata commit

--- scraping/wikidata.py
+++ scraping/wikidata.py
@@ -32,7 +32,8 @@ def wiki_search(search):
 
     R = S.get(url=URL, params=PARAMS)
     DATA = R.json()
-
+    if (len(DATA['query']['search']) == 0):
+        return "Page not found on wikipedia"
 
     # use the first result as the page for a parse
 
@@ -48,23 +49,31 @@ def wiki_search(search):
 
     pagetext = DATA["parse"]["wikitext"]["*"]
 
-    print(pagetext)
-
-    # remove speciesbox
-    paragraph = re.sub('({{(.|\n)+?}}\n\n)', '', pagetext, 1)
-    paragraph = re.search('(\'\'\'(.+\n+)+?(==))', paragraph).group()
-    # remove next paragraph header
-    paragraph = re.sub('(==)', '', paragraph)
-    # parse and remove tags
-    paragraph = re.sub('(<.+?>)', '<<<', paragraph)
-    paragraph = re.sub('(<<<.+<<<)', '', paragraph)
-    # remove quotes and quotes and brackets
-    paragraph = re.sub('(\[([^\[])+\|)', '', paragraph)
-    paragraph = re.sub('(\[)', '', paragraph)
-    paragraph = re.sub('(\])', '', paragraph)
-    paragraph = re.sub('\'', '', paragraph)
-    print(paragraph)
-    return paragraph
+    try:
+        # remove speciesbox
+        paragraph = re.sub('({{(.|\n)+?}}\n)', '', pagetext, 1)
+        paragraph = re.search('(\'\'\'(.+\n+)+?(==))', paragraph).group()
+        # remove next paragraph header
+        paragraph = re.sub('(==)', '', paragraph)
+        # parse and remove tags
+        paragraph = re.sub('(<.+?>)', '<<<', paragraph)
+        paragraph = re.sub('(<<<(.|\n)+?<<<)', '', paragraph)
+        paragraph = re.sub('<<<', '', paragraph)
+        # remove quotes and brackets
+        paragraph = re.sub('\[', '', paragraph)
+        paragraph = re.sub('\]', '', paragraph)
+        paragraph = re.sub('\'', '', paragraph)
+        # parse unclean bracket matching
+        paragraph = re.sub('((.|\n)+?}}\n)', '', paragraph)
+        #remove citation links
+        paragraph = re.sub('({{(c|C)ite.+?}})', '', paragraph)
+        paragraph = re.sub('(\|.+(\n|.))', '', paragraph)
+        print(paragraph)
+        if (paragraph == "") :
+            return "Parsing error. View wikipedia page titled " + PARAMS["page"]
+        return paragraph
+    except AttributeError: 
+        return "Parsing error. View wikipedia page titled " + PARAMS["page"]
 
 # Animal pages tend to have a "The " that will get parsed out by the regex. We can add this in manually if we know we're searching an animal.
 
