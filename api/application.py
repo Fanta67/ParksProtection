ef87931f539db5ddec1d1fe658083a1aa6da4b8a
Roman Kuhn <roman.kuhn1@gmail.com>
2020-04-15 15:58:59 -0500

    added updated uml_diagram.png and finzlied uml_diagram.txt

--- api/application.py
+++ api/application.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_restless import APIManager
-from flask_cors import CORS, cross_origin
 import os
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
@@ -9,8 +8,6 @@ from flask_sqlalchemy import SQLAlchemy
 load_dotenv()
 # Create the Flask application and the Flask-SQLAlchemy object.
 application = Flask(__name__)
-cors = CORS(application)
-application.config['CORS_HEADERS'] = 'Content-Type'
 
 # application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///memory'
 application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + os.environ['DB_USER'] + ':' + os.environ['DB_PASS'] + '@' + os.environ['DB_HOST'] + ':' + os.environ['DB_PORT'] + '/' + os.environ['DB_NAME']
@@ -87,177 +84,90 @@ class ParkStates(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Unicode)
     park_code = db.Column(db.Unicode, db.ForeignKey('parks.code'))
+    
     park = db.relationship(Parks, backref=db.backref("states"))
 
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = 'example.com'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    # Set whatever other headers you like...
-    return response
 
-
-# Create the database tables.
-db.create_all()
+# Search in results
+def search_postprocessor(result=None, search_params=None, **kw):
+    if(search_params != None and "search_query" in search_params):
+        attributes = {'category', 'com_name', 'des', 'duration', 'family', 'family_com', 'growth', 'sci_name', 'status', 'toxicity', 'list_date', 'plan', 'tax_group', 'address', 'code', 'desc', 'designation', 'directions', 'email', 'name', 'phone', 'weather'}
+        keywords = search_params["search_query"].lower().split()
+        if(len(keywords) == 0):
+            keywords = [" "]
+        objects = []
+        for instance in result["objects"]:
+            instanceMatched = False
+            for attribute in instance:
+                if attribute == "states":
+                    for state in instance["states"]:
+                        for keyword in keywords:
+                            if(keyword in state["name"].lower()):
+                                keyword = keyword.upper()
+                                matchSplit = state["name"].split()
+                                match = None
+                                for word in matchSplit:
+                                    if keyword in word:
+                                        match = word.replace(keyword, "<hlt>" + keyword + "</hlt>")
+                                        break
+                                if("id" in instance):
+                                    objects.append({"id": instance["id"], "com_name": instance["com_name"], "image": instance["image"], "match": match})
+                                else:
+                                    objects.append({"code": instance["code"], "name": instance["name"], "image": instance["images"].split()[0], "match": match})
+                                instanceMatched = True
+                                break
+                        if(instanceMatched):
+                            break
+                elif(attribute in attributes):
+                    val = instance[attribute]
+                    if(isinstance(val, str)):
+                        for keyword in keywords:
+                            val = val.lower()
+                            if(keyword in val):
+                                matchSplit = val.split()
+                                match = None
+                                for word in matchSplit:
+                                    if keyword in word:
+                                        match = word.replace(keyword, "<hlt>" + keyword + "</hlt>")
+                                        break
+                                if("id" in instance):
+                                    objects.append({"id": instance["id"], "com_name": instance["com_name"], "image": instance["image"], "match": match})
+                                else:
+                                    objects.append({"code": instance["code"], "name": instance["name"], "image": instance["images"].split()[0], "match": match})
+                                instanceMatched = True
+                                break
+                        if(instanceMatched):
+                            break
+        result["objects"] = objects
+        result["num_results"] = len(objects)
+        del result["page"]
+        del result["total_pages"]
 
 # Create the Flask-Restless API manager.
 manager = APIManager(application, flask_sqlalchemy_db=db)
-
-class Searching:
-    plant_search_terms = None
-
-def plant_preprocessor(search_params=None, **kw):
-    # if there was a search attempt let's handle it, if not leave it alone
-    if(search_params != None and 'search_query' in search_params):
-        # get all the keywords
-        keywords = search_params['search_query'].split()
-        # all the possible attributes we will search through
-        attributes = ['category', 'com_name', 'duration', 'family', 'family_com', 'growth', 'sci_name', 'status', 'toxicity']
-        # define our filters
-        listDicts = []
-
-        for keyword in keywords: # iterate through each keyword
-            keyword = '%' + keyword + '%' # makes it so we can search anywhere in the string
-            for attribute in attributes: # iterate through the columns/attributes
-                new_dict = dict(name=attribute, op='ilike', val=keyword) # make a new filter dict
-                listDicts.append(new_dict) # add it
-                print(listDicts)
-            states_dict = dict(name='states__name', op='any', val=keyword.upper()) # states dict
-            listDicts.append(states_dict)
-
-        search_params["filters"] = [{"or": listDicts}] # set the search_params
-
-    pass # end of function
-
-def animal_preprocessor(search_params=None, **kw):
-
-    # if there was a search attempt let's handle it, if not leave it alone
-    if(search_params != None and 'search_query' in search_params):
-        # get all the keywords
-        keywords = search_params['search_query'].split()
-        # all the possible attributes we will search through
-        attributes = ['com_name', 'des', 'sci_name', 'status', 'tax_group']
-        # define our filters
-        listDicts = []
-
-        for keyword in keywords: # iterate through each keyword
-            keyword = '%' + keyword + '%' # makes it so we can search anywhere in the string
-            for attribute in attributes: # iterate through the columns/attributes
-                new_dict = dict(name=attribute, op='ilike', val=keyword) # make a new filter dict
-                listDicts.append(new_dict) # add it
-                print(listDicts)
-            states_dict = dict(name='states__name', op='any', val=keyword.upper()) # states dict
-            listDicts.append(states_dict)
-
-        search_params["filters"] = [{"or": listDicts}] # set the search_params
-
-    pass # end of function
-
-def park_preprocessor(search_params=None, **kw):
-
-    # if there was a search attempt let's handle it, if not leave it alone
-    if(search_params != None and 'search_query' in search_params):
-        # get all the keywords
-        keywords = search_params['search_query'].split()
-        # all the possible attributes we will search through
-        attributes = ['address', 'code', 'desc', 'designation', 'name', 'weather']
-        # define our filters
-        listDicts = []
-
-        for keyword in keywords: # iterate through each keyword
-            keyword = '%' + keyword + '%' # makes it so we can search anywhere in the string
-            for attribute in attributes: # iterate through the columns/attributes
-                new_dict = dict(name=attribute, op='ilike', val=keyword) # make a new filter dict
-                listDicts.append(new_dict) # add it
-                print(listDicts)
-            states_dict = dict(name='states__name', op='any', val=keyword.upper()) # states dict
-            listDicts.append(states_dict)
-
-        search_params["filters"] = [{"or": listDicts}] # set the search_params
-
-    pass # end of function
-
-
-
-# Create API endpoints, which will be available at /api/<tablename> by
-# default. Allowed HTTP methods can be specified as well.
-from itertools import filterfalse
-def search_process(result=None, **kw):
-    print('POST PROCESSOR CALLED')
-    #print(result)
-    #print('the size of results is')
-    #print(result)
-
-    print('\n\n\n\n\n\n\n')
-    print('plant search terms')
-    print(Searching.plant_search_terms)
-    if(Searching.plant_search_terms != None):
-        print('\n\n\n\n\n\n\n')
-        print('plant search terms is not null, now filtering')
-        def search_filter(plant):
-            #include_result = False
-            for attribute in list(plant):
-                for keyword in Searching.plant_search_terms:
-                    #print('attribute, keyword', attribute, keyword)
-                    if(isinstance(plant[attribute],str) and keyword in plant[attribute]):
-                        print('keeping this one')
-                        return False
-            print('not keeping this one')
-            return True
-        print('results before')
-        print(len(result['objects']))
-        result['objects'][:] = filterfalse(search_filter, result['objects'])
-        print('after')
-        print(len(result['objects']))
-
-
-
-    result['FART'] = 'ASS'
-
-    #print('PAGE 1 ')
-    #result = {'hello':  'world'}
-    #print(result)
-    #print(kw)
-    pass
-    #return result
 
 # Create API endpoints, which will be available at /api/<tablename> by
 # default. Allowed HTTP methods can be specified as well.
 # manager.create_api(Person, methods=['GET', 'POST', 'DELETE'])
 # manager.create_api(Article, methods=['GET'])
 
-
 animals_blueprint = manager.create_api(Animals, methods=['GET'],
-                        preprocessors={'GET_MANY': [animal_preprocessor]},
-                        #postprocessors={'GET_MANY': [search_process]} ,
+                        postprocessors={'GET_MANY': [search_postprocessor]} ,
                         max_results_per_page=1000,
-                        results_per_page=1000
+                        results_per_page=9
                         )
 plants_blueprint = manager.create_api(Plants, methods=['GET'],
-                        preprocessors={'GET_MANY': [plant_preprocessor]},
-                        #postprocessors={'GET_MANY': [search_process]} ,
+                        postprocessors={'GET_MANY': [search_postprocessor]} ,
                         max_results_per_page=1000,
-                        results_per_page=1000
+                        results_per_page=9
                         )
 parks_blueprint = manager.create_api(Parks, methods=['GET'],
-                        preprocessors={'GET_MANY': [park_preprocessor]},
-                        #postprocessors={'GET_MANY': [search_process]} ,
+                        postprocessors={'GET_MANY': [search_postprocessor]} ,
                         max_results_per_page=1000,
-                        results_per_page=1000
+                        results_per_page=9
                         )
 
-'''
-# add the cors header
-animals_blueprint.after_request(add_cors_headers)
-plants_blueprint.after_request(add_cors_headers)
-parks_blueprint.after_request(add_cors_headers)
-
-application.register_blueprint(animals_blueprint)
-application.register_blueprint(plants_blueprint)
-application.register_blueprint(parks_blueprint)
-'''
-
 @application.route('/')
-@cross_origin()
 def index():
     return "Available endpoints: /api/animals /api/plants /api/parks"
 
@@ -270,3 +180,4 @@ application.after_request(add_headers)
 
 # start the flask loop
 application.run()
+
