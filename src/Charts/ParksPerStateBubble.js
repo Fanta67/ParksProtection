4d24d23cee1494325b54fe3867f1a439ea223742
Dylan Kan <dylan.kan67@utexas.edu>
2020-04-26 09:29:11 -0500

    bubble charts dynamic

--- src/Charts/ParksPerStateBubble.js
+++ src/Charts/ParksPerStateBubble.js
@@ -5,62 +5,134 @@ import styled from 'styled-components';
 import * as d3 from "d3";
 import {withFauxDOM} from 'react-faux-dom';
 
+const Div = styled('div')`
+`
+
 class ParksPerStateBubble extends React.Component {
 
-    componentDidMount() {
+    async componentDidMount() {
         const faux = this.props.connectFauxDOM('div', 'chart');
 
-        var dataset = {
-            "children": [{"Name":"AL","Count":9},
-                {"Name":"AR","Count":7},
-                {"Name":"AZ","Count":23},
-                {"Name":"CA","Count":27},
-                {"Name":"CO","Count":12},
-                {"Name":"CT","Count":4},
-                {"Name":"DE","Count":1},
-                {"Name":"FL","Count":11},
-                {"Name":"GA","Count":13},
-                {"Name":"IA","Count":3},
-                {"Name":"ID","Count":7},
-                {"Name":"IL","Count":3},
-                {"Name":"IN","Count":4},
-                {"Name":"KS","Count":6},
-                {"Name":"KY","Count":6},
-                {"Name":"LA","Count":7},
-                {"Name":"MA","Count":18},
-                {"Name":"MD","Count":19},
-                {"Name":"ME","Count":4},
-                {"Name":"MI","Count":6},
-                {"Name":"MN","Count":5},
-                {"Name":"MO","Count":7},
-                {"Name":"MS","Count":10},
-                {"Name":"MT","Count":9},
-                {"Name":"NC","Count":13},
-                {"Name":"ND","Count":4},
-                {"Name":"NE","Count":6},
-                {"Name":"NH","Count":2},
-                {"Name":"NJ","Count":8},
-                {"Name":"NM","Count":14},
-                {"Name":"NV","Count":4},
-                {"Name":"NY","Count":29},
-                {"Name":"OH","Count":12},
-                {"Name":"OK","Count":4},
-                {"Name":"OR","Count":7},
-                {"Name":"PA","Count":23},
-                {"Name":"RI","Count":3},
-                {"Name":"SC","Count":9},
-                {"Name":"SD","Count":7},
-                {"Name":"TN","Count":14},
-                {"Name":"TX","Count":14},
-                {"Name":"UT","Count":13},
-                {"Name":"VA","Count":24},
-                {"Name":"VI","Count":5},
-                {"Name":"VT","Count":2},
-                {"Name":"WA","Count":13},
-                {"Name":"WI","Count":2},
-                {"Name":"WV","Count":8},
-                {"Name":"WY","Count":6}]
-        };
+        const states = ['AL',
+              'AR',
+              'AZ',
+              'CA',
+              'CO',
+              'CT',
+              'DE',
+              'FL',
+              'GA',
+              'IA',
+              'ID',
+              'IL',
+              'IN',
+              'KS',
+              'KY',
+              'LA',
+              'MA',
+              'MD',
+              'ME',
+              'MI',
+              'MN',
+              'MO',
+              'MS',
+              'MT',
+              'NC',
+              'ND',
+              'NE',
+              'NH',
+              'NJ',
+              'NM',
+              'NV',
+              'NY',
+              'OH',
+              'OK',
+              'OR',
+              'PA',
+              'RI',
+              'SC',
+              'SD',
+              'TN',
+              'TX',
+              'UT',
+              'VA',
+              'VI',
+              'VT',
+              'WA',
+              'WI',
+              'WV',
+              'WY'
+        ]
+
+        let url = "https://api.parkprotection.me/api/parks?results_per_page=3&q={\"filters\":[{\"name\":\"states__name\",\"op\":\"any\",\"val\":\"";
+
+        var data = new Map();
+        for(const state of states) {
+            const currurl = url + state + "\"}]}";
+            const response = await fetch(currurl);
+            const data_obj = await response.json();
+            data.set(state, data_obj.num_results);
+        }
+
+        var datamap = [];
+        for (const [key, value] of data.entries()) {
+            const obj = { Name : key, Count: value};
+            datamap.push(obj);
+        }
+
+        var dataset = {"children": datamap}
+
+        // var dataset = {
+        //     "children": [{"Name":"AL","Count":9},
+        //         {"Name":"AR","Count":7},
+        //         {"Name":"AZ","Count":23},
+        //         {"Name":"CA","Count":27},
+        //         {"Name":"CO","Count":12},
+        //         {"Name":"CT","Count":4},
+        //         {"Name":"DE","Count":1},
+        //         {"Name":"FL","Count":11},
+        //         {"Name":"GA","Count":13},
+        //         {"Name":"IA","Count":3},
+        //         {"Name":"ID","Count":7},
+        //         {"Name":"IL","Count":3},
+        //         {"Name":"IN","Count":4},
+        //         {"Name":"KS","Count":6},
+        //         {"Name":"KY","Count":6},
+        //         {"Name":"LA","Count":7},
+        //         {"Name":"MA","Count":18},
+        //         {"Name":"MD","Count":19},
+        //         {"Name":"ME","Count":4},
+        //         {"Name":"MI","Count":6},
+        //         {"Name":"MN","Count":5},
+        //         {"Name":"MO","Count":7},
+        //         {"Name":"MS","Count":10},
+        //         {"Name":"MT","Count":9},
+        //         {"Name":"NC","Count":13},
+        //         {"Name":"ND","Count":4},
+        //         {"Name":"NE","Count":6},
+        //         {"Name":"NH","Count":2},
+        //         {"Name":"NJ","Count":8},
+        //         {"Name":"NM","Count":14},
+        //         {"Name":"NV","Count":4},
+        //         {"Name":"NY","Count":29},
+        //         {"Name":"OH","Count":12},
+        //         {"Name":"OK","Count":4},
+        //         {"Name":"OR","Count":7},
+        //         {"Name":"PA","Count":23},
+        //         {"Name":"RI","Count":3},
+        //         {"Name":"SC","Count":9},
+        //         {"Name":"SD","Count":7},
+        //         {"Name":"TN","Count":14},
+        //         {"Name":"TX","Count":14},
+        //         {"Name":"UT","Count":13},
+        //         {"Name":"VA","Count":24},
+        //         {"Name":"VI","Count":5},
+        //         {"Name":"VT","Count":2},
+        //         {"Name":"WA","Count":13},
+        //         {"Name":"WI","Count":2},
+        //         {"Name":"WV","Count":8},
+        //         {"Name":"WY","Count":6}]
+        // };
 
         var diameter = 600;
         var colors = d3.scaleOrdinal(d3.schemeCategory10);
